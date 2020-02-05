1e6cf4446eb6a097c611079a968c31af7cbcbecc
Dominik Nikiel <dominiknikiel@gmail.com>
2020-02-05 18:20:31 +0100

    Updates

---
+++ precache-manifest.a1427b9f75cd301aaeb2f29ad4016bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda6997fc97b97ed82cc817d76bba845",
+    "url": "/widget-generator/index.html"
+  },
+  {
+    "revision": "d1467dd9928ac64dcda8",
+    "url": "/widget-generator/static/css/main.b3576a7a.chunk.css"
+  },
+  {
+    "revision": "ef0717d2a4321863b9a4",
+    "url": "/widget-generator/static/js/2.5835f64d.chunk.js"
+  },
+  {
+    "revision": "959150e688866e3de05cb62dc4dcd7db",
+    "url": "/widget-generator/static/js/2.5835f64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1467dd9928ac64dcda8",
+    "url": "/widget-generator/static/js/main.7779dcd0.chunk.js"
+  },
+  {
+    "revision": "b7a6afc256453fe21407",
+    "url": "/widget-generator/static/js/runtime-main.1be042e3.js"
+  }
+]);
